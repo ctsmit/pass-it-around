739f65ef1d67b3ff191f55891b0af1db8bb3a106
ctsmit <114516481+ctsmit@users.noreply.github.com>
2022-11-30 14:35:39 -0500

    very nice

--- server.js
+++ server.js
@@ -2,22 +2,25 @@ const app = require("express")()
 const port = 3000
 
 app.get("/", (req, res) => {
-   res.send("99 Bottles of beer on the wall, " + "<a href = '/98'>take one down, pass it around</a>")
+   res.send("99 little bugs in the code<br> 99 little bugs <br> <a href = '/98'>take one down<br> patch it around</a>")
 })
 
 app.get("/0", (req, res) => {
-   res.send("0 Bottles of beer on the wall, " + "<a href = '/'>start over?</a>")
+   res.send("0 bugs? no way<br> <a href = '/'>start over?</a>")
 })
 
 app.get("/:numberOfBottles", (req, res) => {
-   res.send(req.params.numberOfBottles + " Bottles of beer on the wall, <a href = '" + (req.params.numberOfBottles-1)+ "'>take one down, pass it around</a>")
+   let randomNumberOfBottles = Math.floor(Math.random() * 11)
+   let newBottles = Number(req.params.numberOfBottles)
+
+   if (randomNumberOfBottles<10) {
+      newBottles--
+   } else {
+      newBottles += randomNumberOfBottles - Math.floor(Math.random() * 5)
+   }
+   
+   res.send(req.params.numberOfBottles + " little bugs in the code<br>" + req.params.numberOfBottles + " little bugs<br> <a href = '" + newBottles + "'>take one down<br> patch it around</a>")
 })
-
-
-
-
-
-
 
 
 
