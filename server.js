1b5c53f7a1e864ed0e288b1c6182101f55637c96
ctsmit <114516481+ctsmit@users.noreply.github.com>
2022-11-30 13:38:35 -0500

    beerrrrrrrrr

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const app = require("express")()
 const port = 3000
 
 app.get("/", (req, res) => {
-   res.send("99 Bottles of beer on the wall, " + "<a>take one down, pass it around</a>")
+   res.send("99 Bottles of beer on the wall, " + "<a href = '/98'>take one down, pass it around</a>")
 })
+
+app.get("/0", (req, res) => {
+   res.send("0 Bottles of beer on the wall, " + "<a href = '/'>start over?</a>")
+})
+
+app.get("/:numberOfBottles", (req, res) => {
+   res.send(req.params.numberOfBottles + " Bottles of beer on the wall, <a href = '" + (req.params.numberOfBottles-1)+ "'>take one down, pass it around</a>")
+})
+
 
 
 
